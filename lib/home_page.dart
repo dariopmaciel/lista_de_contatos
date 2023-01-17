f47cf2fa78ed2811ddc872cd71ae08333fe501ea
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-01-17 09:09:38 -0300

    atz01

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,17 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:lista_de_contatos/api.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:lista_de_contatos/top_bar.dart';
-import 'package:http/http.dart' as http;
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
         elevation: 0,
-        centerTitle: true,
         title: const Text(
           "Lista de Contatos",
           style: TextStyle(fontSize: 30),
@@ -30,19 +23,14 @@ class _HomePageState extends State<HomePage> {
               IconButton(
                 onPressed: () {},
                 icon: const Icon(Icons.search),
-              ),
+              )
             ],
           )
         ],
+        centerTitle: true,
       ),
       drawer: const Drawer(),
-      body: Column(
-        children: const <Widget>[TopBar()],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+      body: Column(children: const <Widget>[TopBar()]),
     );
   }
 }
